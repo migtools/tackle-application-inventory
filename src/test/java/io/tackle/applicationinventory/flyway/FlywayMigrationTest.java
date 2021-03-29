5e2a806e42dabac2670275b5991ff483ef189e2b
mrizzi <mrizzi@redhat.com>
2021-03-29 17:31:09 +0200

    Added Flyway test with profile

---
+++ src/test/java/io/tackle/applicationinventory/flyway/FlywayMigrationTest.java
@@ -0,0 +1,34 @@
+package io.tackle.applicationinventory.flyway;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+@TestProfile(FlywayMigrationProfile.class)
+public class FlywayMigrationTest {
+    
+    @Inject
+    Flyway flyway;
+    
+    @Test
+    public void testMigration() {
+        // check the number of migrations applied equals the number of files in resources/db/migration folder
+        assertEquals(2, flyway.info().applied().length);
+        // check the current migration version is the one from the last file in resources/db/migration folder
+        assertEquals("20210308.1", flyway.info().current().getVersion().toString());
+        // just a basic test to double check the application started
+        // to prove the flyway scripts ran successfully during startup
+        given()
+            .accept("application/json")
+            .when().get("/q/health/")
+            .then()
+            .statusCode(200);
+    }
+}
