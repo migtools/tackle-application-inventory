3c70296ca4316ae2a2a1fc3bc653468811d67352
Jonathan Vila <jonathan.vila@gmail.com>
2021-06-10 07:41:25 +0200

    [TACKLE-93] Provide data for Adoption Suggestion Plan (#55)
    * Added review filtered list to be able to get a list of reviews for a list of review.id
    
    * Added filterable to the Application sub element of Review
    Added sample data on dev mode
    
    * Fixed test
    
    * Adoption Plan report created based on traversing a Reverse Graph
    Refactored to use a Service and an Enum for effort, and a DTO for the response
    Added more dependencies to test
    
    * Added ServiceTest, ResourceTest for Reports
    
    * Added USAGE documentation for Adoption Plan report
    Changed endpoint to /report/adoptionplan
    
    * Fixed one test data, adding one dependency
    
    * [PR] Change verb of AdoptionPlan to POST body
    Added test to cover non reviewed apps
    
    * [PR] Change verb of AdoptionPlan to POST body
    Added test to cover non reviewed apps
    
    * [PR] Changed the enum to Extra_Large
    Added defense for non existing Enum values and defaulting to 0 effort
    
    * [PR] Applications with a Review.Effort not valid by the Enum are stripped out of the output
    
    * [PR] Adding log info on unmapped Effort
    Removing Awaitility dependency

---
+++ src/main/java/io/tackle/applicationinventory/services/ReportService.java
@@ -0,0 +1,123 @@
+package io.tackle.applicationinventory.services;
+
+import io.quarkus.panache.common.Sort;
+import io.tackle.applicationinventory.dto.AdoptionPlanAppDto;
+import io.tackle.applicationinventory.dto.EffortEstimate;
+import io.tackle.applicationinventory.entities.Application;
+import io.tackle.applicationinventory.entities.ApplicationsDependency;
+import org.jboss.logging.Logger;
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.EdgeReversedGraph;
+import org.jgrapht.graph.builder.GraphTypeBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@ApplicationScoped
+public class ReportService {
+    private static final Logger LOGGER = Logger.getLogger(ReportService.class);
+
+    /*
+        Considering ALL the applications in the system , it will create a Graph
+        and traverse it considering the parent and child dependencies
+        It considers the graph doesn't have a cycle relationship
+         */
+    @Transactional
+    public List<AdoptionPlanAppDto> getAdoptionPlanAppDtos(List<Long> applicationIds) {
+        Graph<Application, DefaultEdge> graph = getApplicationsGraphFromDependencies();
+
+        EdgeReversedGraph<Application, DefaultEdge> reversedGraph = new EdgeReversedGraph<>(graph);
+        List<AdoptionPlanAppDto> sortedList = applicationIds.stream()
+            .map(a -> Application.findById(a))
+            .filter(Objects::nonNull) // Application exists
+            .filter(a -> ((Application) a).review != null) // Application has a review
+            .peek(a -> {
+                if (!EffortEstimate.isExists(((Application) a).review.effortEstimate)) {
+                    LOGGER.warn("Application discarded by missing Effort mapping for : " + ((Application) a).review.effortEstimate);
+                }
+            })
+            .filter(a -> EffortEstimate.isExists(((Application) a).review.effortEstimate)) // The review has a valid Effort
+            .map(e -> buildAdoptionPlanAppDto(applicationIds, reversedGraph, (Application) e))
+            .sorted(Comparator.comparing(e -> String.format("%06d", e.positionY) + e.applicationName))
+            .collect(Collectors.toList());
+
+        // adjusting the positionY value to it's real order
+        sortedList.forEach(e -> e.positionY = sortedList.indexOf(e));
+
+        return sortedList;
+    }
+
+    private Graph<Application, DefaultEdge> getApplicationsGraphFromDependencies() {
+        // create the graph
+        Graph<Application, DefaultEdge> graph = GraphTypeBuilder.<Application, DefaultEdge>
+            directed()
+            .edgeClass(DefaultEdge.class)
+            .allowingSelfLoops(false)
+            .allowingMultipleEdges(false)
+            .weighted(false)
+            .buildGraph();
+
+        // load current data from DB that must be fine (i.e. without cycles) by definition
+        try (Stream<ApplicationsDependency> dependencies = ApplicationsDependency.streamAll(Sort.by("id"))) {
+            dependencies.forEach(applicationsDependency -> Graphs.addEdgeWithVertices(graph, applicationsDependency.from, applicationsDependency.to));
+        }
+        try (Stream<Application> applicationList = Application.streamAll(Sort.by("id"))) {
+            applicationList.forEach(application -> Graphs.addAllVertices(graph, List.of(application)));
+        }
+        return graph;
+    }
+
+    @Transactional
+    private AdoptionPlanAppDto buildAdoptionPlanAppDto(List<Long> applicationIds, EdgeReversedGraph<Application, DefaultEdge> graph, Application application) {
+        if (application.review == null) return null;
+
+        AdoptionPlanAppDto planAppDto = new AdoptionPlanAppDto();
+        planAppDto.applicationName = application.name;
+        planAppDto.decision = application.review.proposedAction;
+        planAppDto.applicationId = application.id;
+        planAppDto.effortEstimate = application.review.effortEstimate;
+        planAppDto.positionY = application.review.workPriority;
+
+        // Obtain the numeric effort from the String stored
+        if (EffortEstimate.isExists(application.review.effortEstimate)) {
+            planAppDto.effort = EffortEstimate.getEnum(application.review.effortEstimate).getEffort();
+        } else {
+            LOGGER.warn("Effort mapping not found for : " + application.review.effortEstimate);
+        }
+
+        // Calculate recursively which is the start X position for this Application depending on its dependencies
+        planAppDto.positionX = startPositionNode(graph, application, applicationIds);
+
+        return planAppDto;
+    }
+
+    private Integer startPositionNode(EdgeReversedGraph<Application, DefaultEdge> graph, Application application, List<Long> selectedApps) {
+        List<Application> parents = Graphs.successorListOf(graph, application);
+        if (parents.size() > 0) {
+            return parents.stream().map(e -> {
+                // recursively we'll find the position calculating previous positions + lengths of the ancestors
+                Integer pos = startPositionNode(graph, e, selectedApps);
+                // if the parent is an application selected to appear in the list then we use its effort, otherwise is like we are hiding it with effort = 0
+                Integer effort;
+                if (EffortEstimate.isExists(e.review.effortEstimate))  {
+                    effort = (selectedApps.contains(e.id)) ? EffortEstimate.getEnum(e.review.effortEstimate).getEffort() : 0;
+                } else {
+                    effort = 0; // to not affect the PositionX
+                }
+                return pos + effort;
+            })
+                .max(Integer::compare).get();
+            // Selecting the largest position from its ancestors
+        } else {
+            // point of return for the recursivity on Top nodes
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/io/tackle/applicationinventory/services/ReportTestUtil.java
@@ -0,0 +1,75 @@
+package io.tackle.applicationinventory.services;
+
+import io.tackle.applicationinventory.entities.Application;
+import io.tackle.applicationinventory.entities.ApplicationsDependency;
+import io.tackle.applicationinventory.entities.Review;
+import io.tackle.commons.tests.SecuredResourceTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public abstract class ReportTestUtil extends SecuredResourceTest {
+    @BeforeEach
+    @Transactional
+    public void insertDataForTests() {
+        for (int i=10; i < 20; i++) {
+            createApplication(i);
+        }
+        createDependency(10, 13);
+        createDependency(10, 12);
+        createDependency(10, 16);
+        createDependency(12, 15);
+        createDependency(13, 12);
+        createDependency(13, 15);
+        createDependency(16, 18);
+        createDependency(16, 19);
+        createDependency(15, 17);
+        createDependency(15, 18);
+        createDependency(17, 19);
+    }
+
+    @AfterEach
+    @Transactional
+    public void deleteDataForTests() {
+        Application.find("name like 'App%'").stream().forEach(e -> e.delete());
+    }
+
+    @Transactional
+    private void createDependency(int from, int to) {
+        ApplicationsDependency dependency = new ApplicationsDependency();
+        dependency.from = (Application) Application.find("name", "App" + from).firstResultOptional().orElseThrow();
+        dependency.to = (Application) Application.find("name", "App" + to).firstResultOptional().orElseThrow();
+        dependency.persist();
+    }
+
+    @Transactional
+    private static void createApplication(int i) {
+        Application app = new Application();
+        app.name = "App" + i;
+        app.businessService = "";
+        app.comments = "";
+        app.description = "";
+        //app.tags = Set.of("tag1", "tag2");
+        app.persistAndFlush();
+
+        Review review = new Review();
+
+        // 10 Small , 11, XLarge, 12 Small, 13 Medium , 14 Small, 15 Large, 16 Large, 17 XLarge, 18 XLarge, 19 XLarge
+        review.effortEstimate = ( List.of(10,12,14).contains(i) ? "Small" : (i == 13 ? "Medium" : (List.of(15,16).contains(i) ? "Large" : "Extra_Large")));
+
+        // 10 Rehost, 11 Refactor, 12 Replatform, 13 Refactor, 14 Rehost, 15 Replatform, 16 Refactor, 17 Rehost, 18 Replatform, 19 Refactor
+        review.proposedAction = ( List.of(10,14,17).contains(i) ? "Rehost" : (List.of(15,12, 18).contains(i) ? "Replatform" : "Refactor"));
+
+        // 10-2 , 11-1, 12-1, 13-1, 14-2, 15-5, 16-6, 17-2, 18-8, 19-9
+        review.workPriority = ( List.of(10,14,17).contains(i) ? 2 : (i == 15 ? 5 : (List.of(13,12,11).contains(i) ? 1 : i - 10)));
+
+        review.businessCriticality = 1;
+        review.comments = "";
+        review.application = app;
+        review.persistAndFlush();
+
+        app.review = review;
+    }
+}
