1a38c7264dd805a23e2011d9d058017870a1e26d
m-brophy <mbrophy@redhat.com>
2021-06-25 13:11:22 +0100

    TACKLE-58 fix forgotten mport class

--- src/main/java/io/tackle/applicationinventory/entities/ApplicationImportForCsv.java
+++ src/main/java/io/tackle/applicationinventory/entities/ApplicationImportForCsv.java
@@ -2,6 +2,7 @@ package io.tackle.applicationinventory.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 @JsonIgnoreProperties({ "createUser", "updateUser", "errorMessage", "valid", "isValid", "filename", "status", "id"})
