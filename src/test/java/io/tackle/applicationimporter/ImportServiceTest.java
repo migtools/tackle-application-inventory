487770d7d11da6281708f0ddce8f13375bdda80f
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-08-13 09:14:06 +0200

    Restore spaces

--- src/test/java/io/tackle/applicationimporter/ImportServiceTest.java
+++ src/test/java/io/tackle/applicationimporter/ImportServiceTest.java
@@ -66,6 +66,8 @@ public class ImportServiceTest extends SecuredResourceTest {
     @Test
     @Order(1)
     protected void testImportServicePost() {
+
+
         ClassLoader classLoader = getClass().getClassLoader();
         File importFile = new File(classLoader.getResource("sample_application_import.csv").getFile());
 
@@ -74,14 +76,15 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .config(RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.JSON)))
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .accept(MediaType.MULTIPART_FORM_DATA)
-                .multiPart("file", importFile)
-                .multiPart("fileName", "sample_application_import.csv")
+                .multiPart("file",importFile)
+                .multiPart("fileName","sample_application_import.csv")
                 .when().post(PATH)
                 .then()
                 .log().all()
                 .statusCode(200).extract().response();
 
         assertEquals(200, response.getStatusCode());
+
 
 
         given()
@@ -95,12 +98,13 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .body("_embedded.'application-import'.size()", is(1));
 
 
+
         Response response2 = given()
                 .config(RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.JSON)))
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .accept(MediaType.MULTIPART_FORM_DATA)
-                .multiPart("file", importFile)
-                .multiPart("fileName", "sample_application_import.csv")
+                .multiPart("file",importFile)
+                .multiPart("fileName","sample_application_import.csv")
                 .when().post(PATH)
                 .then()
                 .log().all()
@@ -118,7 +122,7 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .statusCode(200)
                 .log().body()
                 .body("_embedded.'application-import'.size()", is(1))
-                .extract().path("_embedded.'application-import'[0].'Application Name'").toString());
+        .extract().path("_embedded.'application-import'[0].'Application Name'").toString());
 
         final Long successfulimportApplicationId = Long.valueOf(given()
                 .accept("application/hal+json")
@@ -132,6 +136,7 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .extract().path("_embedded.application[0].id").toString());
 
 
+
         given()
                 .accept(ContentType.JSON)
                 .pathParam("id", successfulimportApplicationId)
@@ -141,6 +146,8 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .statusCode(204);
 
         removeTestObjects(Collections.emptyList());
+
+
     }
 
 //    @Test
@@ -162,12 +169,14 @@ public class ImportServiceTest extends SecuredResourceTest {
 //        removeTestObjects(Collections.emptyList());
 //    }
 
-    protected void createDummyRejectedImports() {
+    protected void createDummyRejectedImports()
+    {
+
         // import 2 applications
         final String multipartPayload = "Record Type 1,Application Name,Description,Comments,Business Service,Tag Type 1,Tag 1,Tag Type 2,Tag 2,Tag Type 3,Tag 3" +
-                ",Tag Type 4,Tag 4,Tag Type 5,Tag 5,Tag Type 6,Tag 6,Tag Type 7,Tag 7,Tag Type 8,Tag 8,Tag Type 9,Tag 9" +
-                ",Tag Type 10,Tag 10,Tag Type 11,Tag 11,Tag Type 12,Tag 12,Tag Type 13,Tag 13,Tag Type 14,Tag 14,Tag Type 15,Tag 15,Tag Type 16,Tag 16" +
-                ",Tag Type 17,Tag 17,Tag Type 18,Tag 18,Tag Type 19,Tag 19,Tag Type 20,Tag 20\n" +
+        ",Tag Type 4,Tag 4,Tag Type 5,Tag 5,Tag Type 6,Tag 6,Tag Type 7,Tag 7,Tag Type 8,Tag 8,Tag Type 9,Tag 9" +
+        ",Tag Type 10,Tag 10,Tag Type 11,Tag 11,Tag Type 12,Tag 12,Tag Type 13,Tag 13,Tag Type 14,Tag 14,Tag Type 15,Tag 15,Tag Type 16,Tag 16" +
+        ",Tag Type 17,Tag 17,Tag Type 18,Tag 18,Tag Type 19,Tag 19,Tag Type 20,Tag 20\n" +
                 "1,,hello,,BS 1,,\n" +
                 "1,  ,,,BS 2,,,,,,,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1,tag type 1,tag 1\n" +
                 "1,name 1,and this,,BS 2,,,,,,mystery tag,,\n" +
@@ -183,27 +192,33 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .post("/file/upload")
                 .then()
                 .statusCode(200);
+
+
     }
 
     @Test
     @Order(2)
     protected void testMultipartImport() {
+
         MultipartImportBody multipartImport = new MultipartImportBody();
         ClassLoader classLoader = getClass().getClassLoader();
         File importFile = new File(classLoader.getResource("sample_application_import.csv").getFile());
         multipartImport.setFilename("testImport");
         multipartImport.setFile(importFile.toString());
 
-        assertEquals(multipartImport.getFileName(), "testImport");
+        assertEquals(multipartImport.getFileName(),"testImport");
 
         removeTestObjects(Collections.emptyList());
+
     }
 
 
     @Test
     @Order(2)
     protected void testMapToApplicationMissingFields() {
+
         createMissingFieldsObjects();
+
 
 
         given()
@@ -218,9 +233,12 @@ public class ImportServiceTest extends SecuredResourceTest {
 
 
         removeTestObjects(Arrays.asList("Test App 1", "Test App 2", "Test App 3", "Test App 4"));
+
     }
 
-    protected void createMissingFieldsObjects() {
+    protected void createMissingFieldsObjects()
+    {
+
         // import 2 applications
         final String multipartPayload = "Record Type 1,Application Name,Description,Comments,Business Service,Tag Type 1,Tag 1,Tag Type 2,Tag 2,Tag Type 3,Tag 3" +
                 ",Tag Type 4,Tag 4,Tag Type 5,Tag 5,Tag Type 6,Tag 6,Tag Type 7,Tag 7,Tag Type 8,Tag 8,Tag Type 9,Tag 9" +
@@ -240,11 +258,14 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .post("/file/upload")
                 .then()
                 .statusCode(200);
+
     }
 
     @Test
     @Order(3)
     protected void testImportServiceNoMatchingTag() {
+
+
         ClassLoader classLoader = getClass().getClassLoader();
         File importFile = new File(classLoader.getResource("sample_application_import.csv").getFile());
 
@@ -254,7 +275,7 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .accept(MediaType.MULTIPART_FORM_DATA)
                 .multiPart("file", importFile)
-                .multiPart("fileName", "sample_application_import.csv")
+                .multiPart("fileName","sample_application_import.csv")
                 .when().post(PATH)
                 .then()
                 .log().all()
@@ -269,6 +290,8 @@ public class ImportServiceTest extends SecuredResourceTest {
     @Test
     @Order(4)
     protected void testImportServiceDuplicatesInFile() {
+
+
         ClassLoader classLoader = getClass().getClassLoader();
         File importFile = new File(classLoader.getResource("duplicate_application_names.csv").getFile());
 
@@ -277,8 +300,8 @@ public class ImportServiceTest extends SecuredResourceTest {
                 .config(RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.JSON)))
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .accept(MediaType.MULTIPART_FORM_DATA)
-                .multiPart("file", importFile)
-                .multiPart("fileName", "duplicate_application_names.csv")
+                .multiPart("file",importFile)
+                .multiPart("fileName","duplicate_application_names.csv")
                 .when().post(PATH)
                 .then()
                 .log().all()
@@ -317,18 +340,21 @@ public class ImportServiceTest extends SecuredResourceTest {
 
         Response r =
                 given()
-                        .accept("text/csv")
-                        .when()
-                        .get("/csv-export?importSummaryId=" + summaryId);
+                .accept("text/csv")
+                .when()
+                .get("/csv-export?importSummaryId=" + summaryId);
+
 
 
         String csv = r.body().print();
         String[] csvFields = csv.split(",");
         List<String> found = Arrays.stream(csvFields).filter("Comments"::equals).collect(Collectors.toList());
-        assertEquals(1, found.size());
+        assertEquals(1,found.size());
+
 
 
         removeTestObjects(Collections.emptyList());
+
     }
 
     @Test
@@ -423,7 +449,8 @@ public class ImportServiceTest extends SecuredResourceTest {
 
     }
 
-    private void removeTestObjects(List<String> appNamesToDelete) {
+    private void removeTestObjects(List<String> appNamesToDelete)
+    {
         ImportSummary[] summaryList =
                 given()
                         .accept("application/json")
@@ -443,10 +470,10 @@ public class ImportServiceTest extends SecuredResourceTest {
 
         ApplicationImport[] importList =
                 given()
-                        .accept("application/json")
-                        .when()
-                        .get("/application-import")
-                        .as(ApplicationImport[].class);
+                .accept("application/json")
+                .when()
+                .get("/application-import")
+                .as(ApplicationImport[].class);
 
 
         Arrays.asList(importList).forEach(thisImport ->
