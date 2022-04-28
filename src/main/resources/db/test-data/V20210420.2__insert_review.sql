7563c177fdb24b8b0c691b8b5a30d1ff44e7d6ac
mrizzi <mrizzi@redhat.com>
2022-04-28 18:11:00 +0200

    TACKLE-507 Update Tackle Repo licenses to APL 2.0

--- src/main/resources/db/test-data/V20210420.2__insert_review.sql
+++ src/main/resources/db/test-data/V20210420.2__insert_review.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright © 2021 Konveyor (https://konveyor.io/)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 INSERT INTO review (id, application_id, proposedAction, effortEstimate, businessCriticality, workPriority, comments, deleted) VALUES (nextval('hibernate_sequence'), 1, 'Retire', 'Large', 11, 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', false);
 INSERT INTO review (id, application_id, proposedAction, effortEstimate, businessCriticality, workPriority, comments, deleted) VALUES (nextval('hibernate_sequence'), 2, 'Rehost', 'Extra large', 1, 21, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', false);
 INSERT INTO review (id, application_id, proposedAction, effortEstimate, businessCriticality, workPriority, comments, deleted) VALUES (nextval('hibernate_sequence'), 3, 'Refactor', 'Small', 5, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', false);
